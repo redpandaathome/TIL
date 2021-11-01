b66e2ddee18a318832f8e8d58c3a5815cc85c7e7
redpandaathome <leeyumi415@gmail.com>
2021-11-01 22:43:00 +0900

    HR-warmup-repeatedString

---
+++ HackerRank/Warm-up/repeatedString.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+function repeatedString(s, n) {
+    // Write your code here
+    //✨✨✨
+    let numberOfAs = [...s].filter(i => i==='a').length
+    let repeatTotal = Math.floor(n/s.length)
+    let extra = n-repeatTotal*s.length
+    let totalAs = numberOfAs*repeatTotal;
+    for(let i=0; i<extra; i++){
+        if(s[i]==='a') totalAs++
+    }
+    return totalAs
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = repeatedString(s, n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
